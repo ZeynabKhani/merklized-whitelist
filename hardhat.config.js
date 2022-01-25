337609146dfa9bec2d0e3dc15b8f2df46b232c20
Zeynab <zkhani9776@gmail.com>
2022-01-25 16:42:16 +0330

    added dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
+require('dotenv').config
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+let privateKey = process.env.PRIVATE_KEY
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/KCJUERkm12wgMiYsJod_I595NsBxMWdC",
+      chainId: 3,
+      account: privateKey
+    }
+  }
 };
